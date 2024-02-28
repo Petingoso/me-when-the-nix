3229328b6789cf2a062bb286f17865b8eb868b47
pet <petingavasco@protonmail.com>
2024-02-28 22:39:48 +0000

    nope

--- home/config/zsh.nix
+++ home/config/zsh.nix
@@ -1,9 +1,10 @@
 {
   pkgs,
   config',
+  config,
   ...
 }: {
-  config.user.users."${config'.username}".shell = pkgs.zsh;
+  config.user.users.${config'.username}.shell = pkgs.zsh;
   home.file.".zshrc".source = ./zsh/zshrc;
   home.file.".p10k.zsh".source = ./zsh/p10k.zsh;
 }
