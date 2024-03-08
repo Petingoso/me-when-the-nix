1a074aa26187cf029f812a8bdde37eb122de3d20
Petingoso <petingavasco@protonmail.com>
2024-03-08 16:07:36 +0000

    added gcc to nvim added diy gruvbox theme

--- home/config/nvim.nix
+++ home/config/nvim.nix
@@ -4,8 +4,13 @@
   ...
 }: {
   home.packages = with pkgs; [nil stylua beautysh clang-tools];
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
+
+  programs.neovim = {
+    defaultEditor = true;
+    enable = true;
+    extraPackages = [pkgs.gcc];
+  };
+
   home.file."./.config/nvim" = {
     source = ./nvim;
     recursive = true;
--- home/home.nix
+++ home/home.nix
@@ -23,6 +23,7 @@
     };
     home.packages = with pkgs; [
       (callPackage ../pkgs/everforest.nix {})
+      (callPackage ../pkgs/gruvbox.nix {})
       papirus-icon-theme
       eza
       fzf
