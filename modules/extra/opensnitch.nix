30d0960969f0bed56d8a7e76922e9d21dd0cf760
Petingoso <petingavasco@protonmail.com>
2024-05-11 22:37:58 +0100

    added kde connect to opensnitch
    open snitch but for real
    
    for real

--- modules/extra/opensnitch.nix
+++ modules/extra/opensnitch.nix
@@ -22,17 +22,30 @@
           operand = "process.path";
           data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-timesyncd";
         };
-      };
-      systemd-resolved = {
-        name = "systemd-resolved";
-        enabled = true;
-        action = "allow";
-        duration = "always";
-        operator = {
-          type = "simple";
-          sensitive = false;
-          operand = "process.path";
-          data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved";
+        systemd-resolved = {
+          name = "systemd-resolved";
+          enabled = true;
+          action = "allow";
+          duration = "always";
+          operator = {
+            type = "simple";
+            sensitive = false;
+            operand = "process.path";
+            data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved";
+          };
+        };
+
+        kde-connect = {
+          name = "kde-connect";
+          enabled = true;
+          action = "allow";
+          duration = "always";
+          operator = {
+            type = "simple";
+            sensitive = true;
+            operand = "process.path";
+            data = "${pkgs.kdeconnect}/libexec/kdeconnectd ";
+          };
         };
       };
     };
