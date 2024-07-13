07eb061aeffa13d9d1053ab2c602e5b7445ba859
Petingoso <petingavasco@protonmail.com>
2024-07-13 22:55:39 +0100

    fixed togglegaps script

--- modules/extra/scripts/togglegaps.nix
+++ modules/extra/scripts/togglegaps.nix
@@ -9,15 +9,14 @@
       text = ''
         #!/usr/bin/env bash
 
-        gaps=$(hyprctl getoption general:gaps_in | grep int | awk '{print $2}')
-
-        if [ $gaps -gt 0 ]
-        then
-            hyprctl keyword general:gaps_in 0
-            hyprctl keyword general:gaps_out 0
+        gaps=$(hyprctl getoption general:gaps_in | grep type ) #custom type: 0 0 0 0
+        digits=$(echo "$gaps" | tr -dc '0-9')
+        if [[ $digits =~ ^[0]+$ ]]; then #if only 0000
+                hyprctl keyword general:gaps_in 5
+                hyprctl keyword general:gaps_out 5
         else
-            hyprctl keyword general:gaps_in 5
-            hyprctl keyword general:gaps_out 5
+                hyprctl keyword general:gaps_in 0
+                hyprctl keyword general:gaps_out 0
         fi
       '';
       executable = true;
