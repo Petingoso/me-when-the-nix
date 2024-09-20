1505107c0270279bfffdc6d10da1b31fd6b0de00
Petingoso <petingavasco@protonmail.com>
2024-09-20 10:06:21 +0100

    removed deprecated option
    removed deprecated option

--- options/home/themes/default.nix
+++ options/home/themes/default.nix
@@ -24,7 +24,7 @@
         };
 
         home.packages = with pkgs; [
-          gnome.adwaita-icon-theme
+          adwaita-icon-theme
           gnome.nixos-gsettings-overrides
           nordic
           (callPackage "${self}/pkgs/everforest.nix" {})
