9dc752c880129154019fb2ba960b4d7703a3641e
Petingoso <petingavasco@protonmail.com>
2024-09-26 10:21:14 +0100

    fixed direnv

--- modules/extra/direnv.nix
+++ modules/extra/direnv.nix
@@ -1,15 +1,8 @@
-{config, lib, ...}: {
-  home-manager.users.${config.mystuff.username} = {
-    programs = {
-      direnv = {
-        enable = true;
-        nix-direnv.enable = true;
-      };
+{config, ...}: {
+  config = {
+    programs.direnv = {
+      enable = true;
+      loadInNixShell = true;
     };
   };
-}
-
-lib.mkIf config.mystuff.zsh.enable {
-home-manager.users.${config.mystuff.username}.programs.direnv.enableZshIntegration = true; # see note on other shells below
-
 }
